c83c513efd4a64c579de37ec4cabf261ac18c03d
MUHAMMAD KHIZAR RIAZ <58303763+khizar7422@users.noreply.github.com>
2023-10-04 20:21:12 +0200

    database setup

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
 import routes from './src/routes/crmRoutes'
+
 const app = express();
 const PORT = 3000;
 
+//mongoose connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://127.0.0.1:27017/CRMdb', {
+ useNewUrlParser: true
+}).then(() => {
+  console.log("connection successfully ");
+})
+
+//bodyparser setup
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 routes(app);
 
@@ -11,6 +25,7 @@ app.get("/", (req, res) =>
 );
 
 app.listen(PORT, () => console.log(`Your server is running on port ${PORT}`));
+
 
 
 
