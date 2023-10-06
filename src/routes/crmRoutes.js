3464a2674d471f6a72a221d2b45c31767a1a39ac
MUHAMMAD KHIZAR RIAZ <58303763+khizar7422@users.noreply.github.com>
2023-10-06 19:50:18 +0200

    specific ID GET endpoint

--- src/routes/crmRoutes.js
+++ src/routes/crmRoutes.js
@@ -1,24 +1,29 @@
-import { addNewContact, getContacts } from "../controllers/crmController";
+import {
+  addNewContact,
+  getContacts,
+  getContactswithID,
+} from "../controllers/crmController";
 const routes = (app) => {
- app.route('/contact')
- .get((req , res, next) => {
-     //middleware
-  console.log (`Request from: ${req.originalUrl}`)
-  console.log (`Request type: ${req.method}`)
-  next();
- 
- }, getContacts)
+  app
+    .route("/contact")
+    //get all contacts
+    .get((req, res, next) => {
+      //middleware
+      console.log(`Request from: ${req.originalUrl}`);
+      console.log(`Request type: ${req.method}`);
+      next();
+    }, getContacts)
 
- .post(addNewContact);
+    //post new contact
+    .post(addNewContact);
 
- app.route('/contact/contactId')
- .put((req , res) =>
- res.send('PUT request successfull')
- )
+  app
+    .route("/contact/contactId")
+    //get specific contact
+    .get(getContactswithID)
+    .put((req, res) => res.send("PUT request successfull"))
 
- .delete((req , res) =>
- res.send('DELETE request successfull')
- )
-}
+    .delete((req, res) => res.send("DELETE request successfull"));
+};
 
 export default routes;
