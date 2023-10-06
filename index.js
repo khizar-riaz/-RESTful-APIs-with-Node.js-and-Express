71d06b99f121e3957c131bb242588b3764c53f81
MUHAMMAD KHIZAR RIAZ <58303763+khizar7422@users.noreply.github.com>
2023-10-06 21:29:17 +0200

    static file serving

--- index.js
+++ index.js
@@ -22,6 +22,11 @@ mongoose.connect('mongodb://127.0.0.1:27017/CRMdb', {
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+//serving static files
+//on browser enter http://localhost:3000/cat.png then it shows that image
+app.use(express.static('public'))
+
+
 routes(app);
 
 app.get("/", (req, res) =>
